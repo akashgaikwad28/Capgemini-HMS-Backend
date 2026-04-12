deafa3fbf52ed2c44f5ab011dcc5b8c9ca53c239
Rahul <mouryarahul543@gmail.com>
2026-04-12 18:45:21 +0530

    Refine AppointmentDTO with Swagger annotations for enhanced API documentation.

--- src/main/java/com/capgemini/hms/appointment/dto/AppointmentDTO.java
+++ src/main/java/com/capgemini/hms/appointment/dto/AppointmentDTO.java
@@ -3,6 +3,7 @@ package com.capgemini.hms.appointment.dto;
 import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
+
 import java.time.LocalDateTime;
 
 public class AppointmentDTO {
