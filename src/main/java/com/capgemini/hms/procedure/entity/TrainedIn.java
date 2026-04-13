9587542e44a43c8fd4ad528998101489aca6c39b
akashgaikwad28 <akash.gaikwad9945@gmail.com>
2026-04-13 09:39:44 +0530

    TrainedIn.java

--- src/main/java/com/capgemini/hms/procedure/entity/TrainedIn.java
+++ src/main/java/com/capgemini/hms/procedure/entity/TrainedIn.java
@@ -8,21 +8,11 @@ import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.MapsId;
 import jakarta.persistence.Table;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
 
 import java.time.LocalDateTime;
 
 @Entity
 @Table(name = "trained_in")
-@Getter
-@Setter
-@NoArgsConstructor
-@AllArgsConstructor
-@Builder
 public class TrainedIn {
 
     @EmbeddedId
@@ -43,4 +33,55 @@ public class TrainedIn {
 
     @Column(name = "certificationexpires", nullable = false)
     private LocalDateTime certificationExpires;
+
+    public TrainedIn() {
+    }
+
+    public TrainedIn(TrainedInId id, Physician physician, Procedure procedure, LocalDateTime certificationDate, LocalDateTime certificationExpires) {
+        this.id = id;
+        this.physician = physician;
+        this.procedure = procedure;
+        this.certificationDate = certificationDate;
+        this.certificationExpires = certificationExpires;
+    }
+
+    public TrainedInId getId() {
+        return id;
+    }
+
+    public void setId(TrainedInId id) {
+        this.id = id;
+    }
+
+    public Physician getPhysician() {
+        return physician;
+    }
+
+    public void setPhysician(Physician physician) {
+        this.physician = physician;
+    }
+
+    public Procedure getProcedure() {
+        return procedure;
+    }
+
+    public void setProcedure(Procedure procedure) {
+        this.procedure = procedure;
+    }
+
+    public LocalDateTime getCertificationDate() {
+        return certificationDate;
+    }
+
+    public void setCertificationDate(LocalDateTime certificationDate) {
+        this.certificationDate = certificationDate;
+    }
+
+    public LocalDateTime getCertificationExpires() {
+        return certificationExpires;
+    }
+
+    public void setCertificationExpires(LocalDateTime certificationExpires) {
+        this.certificationExpires = certificationExpires;
+    }
 }
