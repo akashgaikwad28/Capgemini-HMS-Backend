b90b69c5403434ebe6a1f1bc02af9f614e74bc0b
Rahul <mouryarahul543@gmail.com>
2026-04-14 23:06:57 +0530

    Adds a global model interceptor to all application routes.

---
+++ src/main/java/com/capgemini/hms/config/WebMvcConfig.java
@@ -0,0 +1,18 @@
+package com.capgemini.hms.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.*;
+
+@Configuration
+public class WebMvcConfig implements WebMvcConfigurer {
+
+    @Autowired
+    private GlobalModelInterceptor interceptor;
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(interceptor)
+                .addPathPatterns("/**"); // apply to all routes
+    }
+}
